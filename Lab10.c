a7e129dd6a6ea0833061056be5481bb474c2af0e
F1Gio <44180447+F1Gio@users.noreply.github.com>
2021-05-22 16:35:54 -0700

    Update Lab10.c

--- Lab10.c
+++ Lab10.c
@@ -13,6 +13,10 @@
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
 void TimerOn() {
 	TCCR1B = 0x0B;
 	OCR1A = 125;
