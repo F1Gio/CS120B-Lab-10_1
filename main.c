1be2e52c9ab9f64d4f686e22ffef61fea18095f0
F1Gio <44180447+F1Gio@users.noreply.github.com>
2021-05-23 19:03:22 -0700

    Update main.c

--- main.c
+++ main.c
@@ -209,6 +209,24 @@ int displaySMTick(int state) {
 	return state;
 }
 
+unsigned long int findGCD(unsigned long int a, unsigned long int b) {
+	unsigned long int c;
+	
+	while(1){
+		c = a%b;
+			
+		if (c == 0) {
+			return b;
+		}
+		
+		a = b;
+		b = c;
+	}
+	
+	return 0;
+}
+		
+
 int main() { 
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
@@ -242,10 +260,13 @@ int main() {
 
 	TimerSet(10);
 	TimerOn();
-
+	
+	unsigned long GCD = tasks[0]->period;
 	unsigned short i;
 	while(1) {
 		for (i = 0; i < numTasks; i++) {
+			
+			GCD = findGCD(GCD, tasks[i]->period);
 			if (tasks[i] ->elapsedTime == tasks[i]->period) {
 				tasks[i] -> state = tasks[i]-> TickFct(tasks[i]->state);
 				tasks[i] -> elapsedTime = 0;
